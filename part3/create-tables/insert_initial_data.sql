ce542465733f92836c3a4b1d3f033bcca9b11c29
Mornac <imornac@gmail.com>
2025-07-11 18:00:21 +0200

    fix errors on the file

--- part3/create-tables/insert_initial_data.sql
+++ part3/create-tables/insert_initial_data.sql
@@ -1,16 +1,15 @@
-INSERT INTO `users` (`id`, `email`, `password`, `first_name`, `last_name`, `is_admin`) 
+INSERT INTO users (id, email, password, first_name, last_name, is_admin) 
 VALUES (
     '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
-    admin@hbnb.io,
+    'admin@hbnb.io',
     'admin1234',
     'Admin',
     'HBnB',
     TRUE
 );
 
-INSERT INTO `amenities` (`id`, `name`) 
-VALUES (
+INSERT INTO amenities (id, name) 
+VALUES 
     ('4104a0cc-3128-4639-8cbf-ebe56a32b84a', 'Wifi'),
     ('910da42e-cf77-44d6-8383-18bcb192961c', 'Swimming Pool'),
-    ('a0de5556-c903-49ee-8104-e9a77303c7fc', 'Air Conditioning')
-);
+    ('a0de5556-c903-49ee-8104-e9a77303c7fc', 'Air Conditioning');
